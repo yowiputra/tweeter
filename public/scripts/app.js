e39b16a9e5433037373c8b5feb5be2cd00ae48d9
Yohan Wiputra <yowiputra@gmail.com>
2017-07-12 22:07:28 +0000

    minor styling edits

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -65,7 +65,7 @@ $(document).ready(function(){
     }
   }
 
-  function toggleComposeBox(event) {
+  function toggleComposeBox() {
     $('.new-tweet').slideToggle("slow", () => {
       $('.new-tweet textarea').focus();
     });
