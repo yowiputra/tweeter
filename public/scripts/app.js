96ecf25f4351f8b68bfe4d789f05b3075e521441
Yohan Wiputra <yowiputra@gmail.com>
2017-07-12 16:57:25 +0000

    added thumbnails, more consistent styling

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -48,17 +48,25 @@ $(document).ready(function(){
 
   function createTweetElement(dataObj) {
     var $tweet = $("<article>").addClass("tweet");
+
     // create header and its children
     var header = $("<header>").addClass("tweet-header");
+    header.append($("<img>").addClass("tweet-thumbnail").attr("src", dataObj.user.avatars.small));
     header.append($("<h3>").addClass("tweet-name").text(dataObj.user.name));
-    header.append($("<p>").addClass("tweet-handle").text(dataObj.user.handle));
+    header.append($("<span>").addClass("tweet-handle").text(dataObj.user.handle));
+
     // create main and its children
     var main = $("<main>").addClass("tweet-content");
     main.append($("<p>").addClass("tweet-body").text(dataObj.content.text));
+
     // create footer and its children
     var footer = $("<footer>").addClass("tweet-footer");
     var date = new Date(dataObj.created_at*1000);
-    footer.append($("<p>").addClass("tweet-timestamp").text(date.toUTCString()));
+    footer.append($("<span>").addClass("tweet-timestamp").text(date.toUTCString()));
+    footer.append($("<i>").addClass("fa fa-heart"));
+    footer.append($("<i>").addClass("fa fa-retweet"));
+    footer.append($("<i>").addClass("fa fa-flag"));
+
     // append header, main , footer to tweet
     $tweet.append(header).append(main).append(footer);
     return $tweet;
